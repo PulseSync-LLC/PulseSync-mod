39abf3fc231475655e2fa17f8bc9a2ea8c9024c2
foreA-adoxid <forea.adoxid@gmail.com>
2026-03-08 01:32:34 +0300

    fix: remove triggers

--- src/app/_next/static/chunks/7760-47cfee8bbdb7230d.js
+++ src/app/_next/static/chunks/7760-47cfee8bbdb7230d.js
@@ -13818,107 +13818,108 @@
                         let t = {
                             getData: (0, l.L3)(function* () {
                                 let { dynamicPagesResource: a, modelActionsLogger: r } = (0, l._$)(e);
-                                if (e.loadingState !== u.GuX.PENDING && e.loadingState !== u.GuX.RESOLVE)
-                                    try {
-                                        var s;
-                                        let r;
-                                        if (
-                                            (((e.loadingState = u.GuX.PENDING),
-                                            (s = r =
-                                                e.withTriggersV2
-                                                    ? yield a.getTriggersV2({ anchorIds: Object.values(e$.v) })
-                                                    : yield a.getTriggers({ anchorIds: Object.values(e$.v) })) &&
-                                                s.triggers &&
-                                                Array.isArray(s.triggers) &&
-                                                s.triggers.every(
-                                                    (e) =>
-                                                        !!(
-                                                            e &&
-                                                            'object' == typeof e &&
-                                                            'anchorId' in e &&
-                                                            'triggers' in e &&
-                                                            Array.isArray(e.triggers) &&
-                                                            e.triggers.every(
-                                                                (e) =>
-                                                                    !!(
-                                                                        e &&
-                                                                        'object' == typeof e &&
-                                                                        'screenId' in e &&
-                                                                        'feedbackToken' in e &&
-                                                                        'data' in e &&
-                                                                        'meta' in e
-                                                                    ),
-                                                            )
-                                                        ),
-                                                ))
-                                                ? (e.list = ((e) => {
-                                                      let t = [],
-                                                          a = [];
-                                                      return (
-                                                          e.triggers.forEach((e) => {
-                                                              var r;
-                                                              switch (null == (r = e.triggers[0]) ? void 0 : r.meta.notificationId) {
-                                                                  case i.BAR_BELOW:
-                                                                      return void e.triggers.forEach((e) => {
-                                                                          t.push((0, eZ.S3)(e));
-                                                                      });
-                                                                  case i.FULLSCREEN:
-                                                                      return void e.triggers.forEach((e) => {
-                                                                          a.push((0, eJ.fZ)(e));
-                                                                      });
-                                                              }
-                                                          }),
-                                                          (0, l.wg)({ barBelow: { list: t }, modal: { list: a } })
-                                                      );
-                                                  })(r))
-                                                : r &&
-                                                  r.triggers &&
-                                                  Array.isArray(r.triggers) &&
-                                                  r.triggers.every(
-                                                      (e) =>
-                                                          !!(
-                                                              e &&
-                                                              'object' == typeof e &&
-                                                              'anchorId' in e &&
-                                                              'screenId' in e &&
-                                                              'div' in e &&
-                                                              'meta' in e &&
-                                                              !('triggers' in e)
-                                                          ),
-                                                  ) &&
-                                                  (e.list = ((e) => {
-                                                      let t = [],
-                                                          a = [];
-                                                      return (
-                                                          e.triggers.forEach((e) => {
-                                                              switch (e.meta.notificationId) {
-                                                                  case i.BAR_BELOW:
-                                                                      t.push((0, eZ.S3)(e));
-                                                                      return;
-                                                                  case i.FULLSCREEN:
-                                                                      a.push((0, eJ.fZ)(e));
-                                                                      return;
-                                                              }
-                                                          }),
-                                                          (0, l.wg)({ barBelow: { list: t }, modal: { list: a } })
-                                                      );
-                                                  })(r)),
-                                            e.loadingState !== u.GuX.IDLE && (e.loadingState = u.GuX.RESOLVE),
-                                            !e.list)
-                                        )
-                                            return;
-                                        let { barBelow: n, modal: o } = e.list;
-                                        n.setAnchorId(e$.v.ON_START_BAR_BELOW),
-                                            n.barBelowItem && (n.show(), t.shown(n.barBelowItem.anchorId, n.barBelowItem.screenId, n.barBelowItem.feedbackToken)),
-                                            o.setAnchorId(e$.v.ON_START_FULLSCREEN),
-                                            o.modalItem && (o.open(), t.shown(o.modalItem.anchorId, o.modalItem.screenId, o.modalItem.feedbackToken));
-                                    } catch (t) {
-                                        r.error(t),
-                                            t instanceof eg.GX &&
-                                                (t.statusCode === eg.X1.NOT_FOUND || t.statusCode === eg.X1.BAD_REQUEST) &&
-                                                (e.errorStatusCode = eg.X1.NOT_FOUND),
-                                            e.loadingState !== u.GuX.IDLE && (e.loadingState = u.GuX.REJECT);
-                                    }
+                                return (e.loadingState = u.GuX.RESOLVE);
+                                // if (e.loadingState !== u.GuX.PENDING && e.loadingState !== u.GuX.RESOLVE)
+                                //     try {
+                                //         var s;
+                                //         let r;
+                                //         if (
+                                //             (((e.loadingState = u.GuX.PENDING),
+                                //             (s = r =
+                                //                 e.withTriggersV2
+                                //                     ? yield a.getTriggersV2({ anchorIds: Object.values(e$.v) })
+                                //                     : yield a.getTriggers({ anchorIds: Object.values(e$.v) })) &&
+                                //                 s.triggers &&
+                                //                 Array.isArray(s.triggers) &&
+                                //                 s.triggers.every(
+                                //                     (e) =>
+                                //                         !!(
+                                //                             e &&
+                                //                             'object' == typeof e &&
+                                //                             'anchorId' in e &&
+                                //                             'triggers' in e &&
+                                //                             Array.isArray(e.triggers) &&
+                                //                             e.triggers.every(
+                                //                                 (e) =>
+                                //                                     !!(
+                                //                                         e &&
+                                //                                         'object' == typeof e &&
+                                //                                         'screenId' in e &&
+                                //                                         'feedbackToken' in e &&
+                                //                                         'data' in e &&
+                                //                                         'meta' in e
+                                //                                     ),
+                                //                             )
+                                //                         ),
+                                //                 ))
+                                //                 ? (e.list = ((e) => {
+                                //                       let t = [],
+                                //                           a = [];
+                                //                       return (
+                                //                           e.triggers.forEach((e) => {
+                                //                               var r;
+                                //                               switch (null == (r = e.triggers[0]) ? void 0 : r.meta.notificationId) {
+                                //                                   case i.BAR_BELOW:
+                                //                                       return void e.triggers.forEach((e) => {
+                                //                                           t.push((0, eZ.S3)(e));
+                                //                                       });
+                                //                                   case i.FULLSCREEN:
+                                //                                       return void e.triggers.forEach((e) => {
+                                //                                           a.push((0, eJ.fZ)(e));
+                                //                                       });
+                                //                               }
+                                //                           }),
+                                //                           (0, l.wg)({ barBelow: { list: t }, modal: { list: a } })
+                                //                       );
+                                //                   })(r))
+                                //                 : r &&
+                                //                   r.triggers &&
+                                //                   Array.isArray(r.triggers) &&
+                                //                   r.triggers.every(
+                                //                       (e) =>
+                                //                           !!(
+                                //                               e &&
+                                //                               'object' == typeof e &&
+                                //                               'anchorId' in e &&
+                                //                               'screenId' in e &&
+                                //                               'div' in e &&
+                                //                               'meta' in e &&
+                                //                               !('triggers' in e)
+                                //                           ),
+                                //                   ) &&
+                                //                   (e.list = ((e) => {
+                                //                       let t = [],
+                                //                           a = [];
+                                //                       return (
+                                //                           e.triggers.forEach((e) => {
+                                //                               switch (e.meta.notificationId) {
+                                //                                   case i.BAR_BELOW:
+                                //                                       t.push((0, eZ.S3)(e));
+                                //                                       return;
+                                //                                   case i.FULLSCREEN:
+                                //                                       a.push((0, eJ.fZ)(e));
+                                //                                       return;
+                                //                               }
+                                //                           }),
+                                //                           (0, l.wg)({ barBelow: { list: t }, modal: { list: a } })
+                                //                       );
+                                //                   })(r)),
+                                //             e.loadingState !== u.GuX.IDLE && (e.loadingState = u.GuX.RESOLVE),
+                                //             !e.list)
+                                //         )
+                                //             return;
+                                //         let { barBelow: n, modal: o } = e.list;
+                                //         n.setAnchorId(e$.v.ON_START_BAR_BELOW),
+                                //             n.barBelowItem && (n.show(), t.shown(n.barBelowItem.anchorId, n.barBelowItem.screenId, n.barBelowItem.feedbackToken)),
+                                //             o.setAnchorId(e$.v.ON_START_FULLSCREEN),
+                                //             o.modalItem && (o.open(), t.shown(o.modalItem.anchorId, o.modalItem.screenId, o.modalItem.feedbackToken));
+                                //     } catch (t) {
+                                //         r.error(t),
+                                //             t instanceof eg.GX &&
+                                //                 (t.statusCode === eg.X1.NOT_FOUND || t.statusCode === eg.X1.BAD_REQUEST) &&
+                                //                 (e.errorStatusCode = eg.X1.NOT_FOUND),
+                                //             e.loadingState !== u.GuX.IDLE && (e.loadingState = u.GuX.REJECT);
+                                //     }
                             }),
                             shown: (0, l.L3)(function* (t, a, i) {
                                 let { dynamicPagesResource: r, modelActionsLogger: s } = (0, l._$)(e);
