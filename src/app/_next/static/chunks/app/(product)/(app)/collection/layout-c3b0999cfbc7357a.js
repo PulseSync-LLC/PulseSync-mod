bd8532b38273bbc69ad2fad254517975669c0011
foreA-adoxid <forea.adoxid@gmail.com>
2026-07-25 02:41:03 +0300

    migration: apply css patches

--- src/app/_next/static/chunks/app/(product)/(app)/collection/layout-c3b0999cfbc7357a.js
+++ src/app/_next/static/chunks/app/(product)/(app)/collection/layout-c3b0999cfbc7357a.js
@@ -207,6 +207,7 @@
                     (e.WebNextNewWaveTabFeedbackForm = 'WebNextNewWaveTabFeedbackForm'),
                     (e.WebNextNdaLabelOnWaveTab = 'WebNextNdaLabelOnWaveTab'),
                     (e.WebNextPaidPerformancePaywallTopSection = 'WebNextPaidPerformancePaywallTopSection'),
+                    (e.WebNextPlayerBarYellowButton = 'WebNextPlayerBarYellowButton'),
                     (e.WebNextPlusOptionsSidebar = 'WebNextPlusOptionsSidebar'),
                     (e.WebNextResetWaveSeeds = 'WebNextResetWaveSeeds'),
                     (e.WebNextConcertsIdentityEventType = 'WebNextConcertsIdentityEventType'),
