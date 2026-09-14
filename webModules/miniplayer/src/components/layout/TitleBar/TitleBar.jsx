35fd9f9bbf7b6cf60d65df4bd551afba780d57a9
foreA.adoxid <forea.adoxid@gmail.com>
2026-09-14 04:48:29 +0300

    feat(miniplayer): add macOS support with native window controls

--- webModules/miniplayer/src/components/layout/TitleBar/TitleBar.jsx
+++ webModules/miniplayer/src/components/layout/TitleBar/TitleBar.jsx
@@ -3,11 +3,13 @@ import TitleBarButton from '../../ui/TitleBarButton/TitleBarButton.jsx'
 import './TitleBar.css'
 
 export default function TitleBar() {
+    const isMacOS = window.PLATFORM === 'darwin'
+
     return (
-        <header className="TitleBar">
+        <header className={`TitleBar${isMacOS ? ' TitleBar_macos' : ''}`}>
             <div className="TitleBar_button_container">
                 <TitleBarButton variant="pin" />
-                <TitleBarButton variant="quit" />
+                {!isMacOS && <TitleBarButton variant="quit" />}
             </div>
         </header>
     )
